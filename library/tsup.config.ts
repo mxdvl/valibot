38f27d2614b356f335c90897ff7b41a65176a6a2
Max Duval <hi@mxdvl.com>
2024-02-03 18:35:01 +0000

    feat: keep source tree structure

--- library/tsup.config.ts
+++ library/tsup.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
+  entry: ['./src/**/*.ts'], // get all source files!
+  bundle: false, // do not bundle them… let the consumers do that
   clean: true,
   format: ['esm', 'cjs'],
-  dts: true,
+  dts: false, // this fails https://github.com/egoist/tsup/issues/920
   outDir: './dist',
 });
