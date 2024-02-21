e1cb545b5ad3e76a3293bb2b41cdbe993ba06dc7
Max Duval <max.duval@theguardian.com>
2024-02-21 15:37:44 +0000

    WIP

--- library/src/index.ts
+++ library/src/index.ts
@@ -1,9 +1,23 @@
-export * from './error/index.ts';
-export * from './methods/index.ts';
-export * from './regex.ts';
-export * from './schemas/index.ts';
-export * from './storages/index.ts';
-export * from './transformations/index.ts';
-export * from './types/index.ts';
-export * from './utils/index.ts';
-export * from './validations/index.ts';
+import * as errors from './error/index.ts';
+import * as methods from './methods/index.ts';
+import * as regex from './regex.ts';
+import * as schemas from './schemas/index.ts';
+import * as storages from './storages/index.ts';
+import * as transformations from './transformations/index.ts';
+import * as types from './types/index.ts';
+import * as utils from './utils/index.ts';
+import * as validations from './validations/index.ts';
+
+export const v = {
+  ...errors,
+  ...methods,
+  ...regex,
+  ...schemas,
+  ...storages,
+  ...transformations,
+  ...types,
+  ...utils,
+  ...validations,
+};
+
+export default v;
